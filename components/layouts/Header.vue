e3037b30e337c29a755ab1053fbae0043765f2bf
iambasanta <sangpangbasanta@gmail.com>
2021-02-13 19:41:26 +0545

    adds nav bar

---
+++ components/layouts/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav
+    class="p-4 shadow-sm bg-gradient-to-r from-green-400 to-blue-500 text-white font-semibold"
+  >
+    <div class="flex justify-between">
+      <a href="">Home</a>
+      <a href="" @click.prevent="logout">Logout</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
