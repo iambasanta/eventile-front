c04325917bfbe06d401c8fd6b9f99facfd02d9eb
iambasanta <sangpangbasanta@gmail.com>
2021-02-13 13:33:52 +0545

    contains useful components

---
+++ components/util/ErrorField.vue
@@ -0,0 +1,22 @@
+<template>
+  <span v-if="errors[field]" class="w-9/12 mt-2 text-red-500 text-semibold"
+    >{{ errors[field][0] }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    errors: {
+      type: Object,
+      default: () => ({}),
+    },
+    field: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style></style>
